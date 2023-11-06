689acbebd100629aa05f4144587fb6a9a13faa8e
jonckheerethijs <jonckheerethijs@gmail.com>
2023-11-06 23:29:24 +0100

    work on functionality for the calculator
    added key event listeners as well as button listeners

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
-function add(x,y){
-    return x+y
+let first = ""
+let second = ""
+let operation = ""
+
+const numberButtons = document.querySelectorAll(".number")
+const operationButtons = document.querySelectorAll(".operator")
+const equalsButton = document.getElementById("equals")
+const clearButton = document.getElementById("all-clear")
+const backspaceButton = document.getElementById("backspace")
+const pointButton = document.getElementById("dot")
+const lastOperationScreen = document.getElementById("result")
+const currentOperationScreen = document.getElementById("user-input")
+
+window.addEventListener("keydown", handleKeyboardInput)
+equalsButton.addEventListener("click", evaluate)
+clearButton.addEventListener("click", clear)
+backspaceButton.addEventListener("click", backspace)
+pointButton.addEventListener("click", addPoint)
+
+function handleKeyboardInput(e) {
+    console.log(`Key ${e.key} pressed!`)
+    if (e.key >= 0 && e.key <= 9) appendNumber(e.key)
+    if (e.key === ".") addPoint()
+    if (e.key === "Backspace") backspace()
+    if (e.key === "Delete" || e.key === "Escape") clear()
+    if (e.key === "=" || e.key === "Enter") evaluate()
+    if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/" || e.key === ":") operate(e.key)
 }
 
-function subtract(x,y){
-    return x-y
+function appendNumber(number){
+    currentOperationScreen.textContent += number
 }
 
-function multiply(x,y){
-    return x*y
+function addPoint(){
+    currentOperationScreen.textContent += "."
 }
 
-function divide(x,y){
-    return x/y
+function add(x, y) {
+    return x + y
 }
 
-function operate(operator, first, second){
-    return window[operator](first, second)
+function subtract(x, y) {
+    return x - y
 }
 
-console.log(operate("subtract",1, 6))
+function multiply(x, y) {
+    return x * y
+}
+
+function divide(x, y) {
+    return x / y
+}
+
+function operate(operation, first, second) {
+    return window[operation](first, second)
+}
+
+console.log(operate("subtract", 1, 6))
